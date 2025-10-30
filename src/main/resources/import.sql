c2535cd0e51f95d10d585fe9c2c8bc7e7075e5e8
Aron Ricardo <aronricardo@hotmail.com>
2025-10-29 21:44:50 -0300

    chore: insert tables

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_endereco(cep, logradouro, bairro, localidade, uf,id_endereco) VALUES("48120000, rua i", "inocoop","sei la", "ba", 1)
+INSERT INTO tb_aluno (nome, data_nascimento, cpf, curso, id)VALUES ('Aron', '1993-03-03', '06083506565', ' TECNICO_ADMINISTRACAO')
