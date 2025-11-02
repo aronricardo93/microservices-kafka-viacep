c7c70d618f6d8d9206c5dd286763f96b42b6351a
Aron Ricardo <aronricardo@hotmail.com>
2025-11-02 10:46:41 -0300

    refactor: reajuste nos inserts

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_endereco(cep, logradouro, bairro, localidade, uf,id_endereco) VALUES("48120000, rua i", "inocoop","sei la", "ba", 1)
-INSERT INTO tb_aluno (nome, data_nascimento, cpf, curso, id)VALUES ('Aron', '1993-03-03', '06083506565', ' TECNICO_ADMINISTRACAO')
