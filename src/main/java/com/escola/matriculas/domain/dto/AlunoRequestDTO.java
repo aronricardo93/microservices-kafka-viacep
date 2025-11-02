f7a2c716e452a37f9e7c13b959f4d217250432f2
Aron Ricardo <aronricardo@hotmail.com>
2025-11-02 10:44:24 -0300

    refactor: validações

--- src/main/java/com/escola/matriculas/domain/dto/AlunoRequestDTO.java
+++ src/main/java/com/escola/matriculas/domain/dto/AlunoRequestDTO.java
@@ -1,11 +1,12 @@
 package com.escola.matriculas.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record AlunoRequestDTO(
-    @NotNull(message = "O nome deve ser informado!") String nome,
-    @NotNull(message = "A data de nascimento deve ser informada!") String dataNascimento,
-    @NotNull(message = "O CPF deve ser informado!") @CPF(message = "CPF inválido!") String cpf,
-    @NotNull(message = "O CEP deve ser informado!") String cep
+    @NotBlank(message = "O nome deve ser informado!") String nome,
+    @NotBlank(message = "A data de nascimento deve ser informada!") String dataNascimento,
+    @NotBlank(message = "O CPF deve ser informado!") @CPF(message = "CPF inválido!") String cpf,
+    @NotBlank(message = "O CEP deve ser informado!") String cep
 ) {}
